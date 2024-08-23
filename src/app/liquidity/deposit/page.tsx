40785430e44ec9a62b5bc24e8b66156ec0ce57d2
Kingsley Victor <javaprodigy56@gmail.com>
2024-08-23 19:24:34 +0100

    made updates

--- src/app/liquidity/deposit/page.tsx
+++ src/app/liquidity/deposit/page.tsx
@@ -4,8 +4,11 @@ import BearIcon from "@/assets/images/Bera.png";
 import MoniIcon from "@/assets/images/logo.svg";
 import { ChipBadge } from "@/components/ui/chipBadge";
 import { useGetTokenLists } from "@/hooks/api/tokens";
+import { useSinglePoolInfo } from "@/hooks/graphql/core";
+import { useTimeInMotion } from "@/hooks/misc";
 import { useProtocolCore } from "@/hooks/onchain/core";
 import { TokenType } from "@/types";
+import { toSF } from "@/utils/format";
 import { faInfo, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Avatar, Select, SelectItem } from "@nextui-org/react";
@@ -50,6 +53,14 @@ export default function Page() {
         [volatilePoolAddress],
     );
 
+    const useStablePoolQuery = useSinglePoolInfo(stablePoolAddress);
+    const { data: stablePool } = useStablePoolQuery();
+
+    const useVolatilePoolQuery = useSinglePoolInfo(volatilePoolAddress);
+    const { data: volatilePool } = useVolatilePoolQuery();
+
+    const timeInMotion = useTimeInMotion();
+
     return (
         <div className="mx-auto flex max-w-[1300px] flex-col gap-10 px-5 pb-10 pt-10 md:pt-20 lg:pt-36">
             <p className="text-2xl">Deposit Liquidity</p>
@@ -77,6 +88,10 @@ export default function Page() {
                         {tokenLists.map((token, index) => {
                             return (
                                 <SelectItem
+                                    isReadOnly={
+                                        selectedTokens[0] === token ||
+                                        selectedTokens[1] === token
+                                    }
                                     onPress={() =>
                                         setSelectedTokens([
                                             token,
@@ -121,6 +136,10 @@ export default function Page() {
                         {tokenLists.map((token, index) => {
                             return (
                                 <SelectItem
+                                    isReadOnly={
+                                        selectedTokens[0] === token ||
+                                        selectedTokens[1] === token
+                                    }
                                     key={index}
                                     onPress={() =>
                                         setSelectedTokens([
@@ -143,67 +162,174 @@ export default function Page() {
                 </div>
             </div>
 
-            <div className="flex gap-3 bg-footer px-5 py-3 text-textgray md:items-center">
-                <span className="flex h-[20px] w-[20px] flex-shrink-0 items-center justify-center rounded-full border border-textgray">
-                    <FontAwesomeIcon icon={faInfo} size="xs" />
-                </span>
-                Start by selecting the tokens. The liquidity pools available for
-                deposit will show up next.
-            </div>
-
-            <div className="flex flex-col gap-5">
-                <p>Available Pools</p>
-                <div className="bg-footer p-5">
-                    <div className="flex flex-col justify-between gap-5 lg:flex-row lg:gap-0">
-                        <div className="flex items-center">
-                            <div className="flex items-center">
-                                <Image src={BearIcon} alt="icon" width={30} />
-                                <Image
-                                    src={MoniIcon}
-                                    alt="icon"
-                                    width={30}
-                                    className="-translate-x-3"
-                                />
-                            </div>
-
-                            <div>
-                                <p>vAMM-MONI/BERA</p>
-                                <ChipBadge>Basic Volatile · 1.0%</ChipBadge>
-                            </div>
-                        </div>
-
-                        <div className="flex justify-between lg:flex-col lg:text-right">
-                            <p className="text-textgray">TVL</p>
-                            <p>$9,062,352.53</p>
-                        </div>
-
-                        <div className="flex justify-between lg:flex-col lg:text-right">
-                            <p className="text-textgray">Fees (24H)</p>
-                            <p>$9,062,352.53</p>
-                        </div>
-
-                        <div className="flex justify-between lg:flex-col lg:text-right">
-                            <p className="text-textgray">Volume (24H)</p>
-                            <p>$9,062,352.53</p>
-                        </div>
-
-                        <div className="flex justify-between lg:flex-col lg:text-right">
-                            <p className="text-textgray">APR (24H)</p>
-                            <p>$9,062,352.53</p>
-                        </div>
-
-                        <div>
-                            <Button className="w-full min-w-0 text-btn-primary">
-                                <FontAwesomeIcon icon={faPlus} />{" "}
-                                <span className="lg:hidden">Add Liquidity</span>
-                            </Button>
-                        </div>
-                    </div>
+            {(selectedTokens[0] === null || selectedTokens[1] === null) && (
+                <div className="flex gap-3 bg-footer px-5 py-3 text-textgray md:items-center">
+                    <span className="flex h-[20px] w-[20px] flex-shrink-0 items-center justify-center rounded-full border border-textgray">
+                        <FontAwesomeIcon icon={faInfo} size="xs" />
+                    </span>
+                    Start by selecting the tokens. The liquidity pools available
+                    for deposit will show up next.
                 </div>
+            )}
+
+            <div className="flex flex-col gap-5">
+                <h5>Available Pools</h5>
+                {(typeof stablePool !== "undefined" ||
+                    typeof volatilePool !== "undefined") &&
+                (stablePool !== null || volatilePool !== null) &&
+                selectedTokens[0] !== null &&
+                selectedTokens[1] !== null ? (
+                    <>
+                        {!!stablePool && (
+                            <div className="bg-footer p-5">
+                                <div className="flex flex-col justify-between gap-5 lg:flex-row lg:gap-0">
+                                    <div className="flex items-center">
+                                        <div className="flex items-center">
+                                            <Image
+                                                src={selectedTokens[0].logoURI}
+                                                alt="icon"
+                                                width={30}
+                                            />
+                                            <Image
+                                                src={selectedTokens[1].logoURI}
+                                                alt="icon"
+                                                width={30}
+                                                className="-translate-x-3"
+                                            />
+                                        </div>
+
+                                        <div>
+                                            <span>
+                                                sAMM-{stablePool.token0.symbol}/
+                                                {stablePool.token1.symbol}
+                                            </span>
+                                            <ChipBadge>
+                                                Basic Stable · 1.0%
+                                            </ChipBadge>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex justify-between lg:flex-col lg:text-right">
+                                        <p className="text-textgray">TVL</p>
+                                        <p>${toSF(stablePool.volumeUSD)}</p>
+                                    </div>
+
+                                    <div className="flex justify-between lg:flex-col lg:text-right">
+                                        <p className="text-textgray">Fees</p>
+                                        <p>${toSF(stablePool.feesUSD)}</p>
+                                    </div>
+
+                                    <div className="flex justify-between lg:flex-col lg:text-right">
+                                        <p className="text-textgray">
+                                            Volume (24H)
+                                        </p>
+                                        <p>
+                                            $
+                                            {toSF(
+                                                stablePool.pairHourData?.findLast(
+                                                    (p) =>
+                                                        timeInMotion -
+                                                            p.hourStartUnix <=
+                                                        24,
+                                                )?.hourlyVolumeUSD,
+                                            )}
+                                        </p>
+                                    </div>
+
+                                    <div>
+                                        <Button className="w-full min-w-0 text-btn-primary">
+                                            <FontAwesomeIcon icon={faPlus} />{" "}
+                                            <span className="lg:hidden">
+                                                Add Liquidity
+                                            </span>
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+
+                        {!!volatilePool && (
+                            <div className="bg-footer p-5">
+                                <div className="flex flex-col justify-between gap-5 lg:flex-row lg:gap-0">
+                                    <div className="flex items-center">
+                                        <div className="flex items-center">
+                                            <Image
+                                                src={selectedTokens[0].logoURI}
+                                                alt="icon"
+                                                width={30}
+                                            />
+                                            <Image
+                                                src={selectedTokens[1].logoURI}
+                                                alt="icon"
+                                                width={30}
+                                                className="-translate-x-3"
+                                            />
+                                        </div>
+
+                                        <div>
+                                            <span>
+                                                vAMM-
+                                                {volatilePool.token0.symbol}/
+                                                {volatilePool.token1.symbol}
+                                            </span>
+                                            <ChipBadge>
+                                                Basic Volatile · 1.0%
+                                            </ChipBadge>
+                                        </div>
+                                    </div>
+
+                                    <div className="flex justify-between lg:flex-col lg:text-right">
+                                        <p className="text-textgray">TVL</p>
+                                        <p>${toSF(volatilePool.volumeUSD)}</p>
+                                    </div>
+
+                                    <div className="flex justify-between lg:flex-col lg:text-right">
+                                        <p className="text-textgray">Fees</p>
+                                        <p>${toSF(volatilePool.feesUSD)}</p>
+                                    </div>
+
+                                    <div className="flex justify-between lg:flex-col lg:text-right">
+                                        <p className="text-textgray">
+                                            Volume (24H)
+                                        </p>
+                                        <p>
+                                            $
+                                            {toSF(
+                                                volatilePool.pairHourData?.findLast(
+                                                    (p) =>
+                                                        timeInMotion -
+                                                            p.hourStartUnix <=
+                                                        24,
+                                                )?.hourlyVolumeUSD,
+                                            )}
+                                        </p>
+                                    </div>
+
+                                    <div>
+                                        <Button className="w-full min-w-0 text-btn-primary">
+                                            <FontAwesomeIcon icon={faPlus} />{" "}
+                                            <span className="lg:hidden">
+                                                Add Liquidity
+                                            </span>
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </>
+                ) : (
+                    <div className="flex gap-3 bg-footer px-5 py-3 text-textgray md:items-center">
+                        <span className="flex h-[20px] w-[20px] flex-shrink-0 items-center justify-center rounded-full border border-textgray">
+                            <FontAwesomeIcon icon={faInfo} size="xs" />
+                        </span>
+                        There are no available liquidity pools. Try supplying
+                        liquidity to low LPs.
+                    </div>
+                )}
             </div>
 
             <div className="flex flex-col gap-5">
-                <p>Low Liquidity Pools</p>
+                <h5>Low Liquidity Pools</h5>
                 <div className="bg-footer p-5">
                     <div className="flex flex-col justify-between gap-5 lg:flex-row lg:gap-0">
                         <div className="flex items-center">
