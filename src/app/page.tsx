66766208babcf9998f6b143de55b86b92f55e693
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-09-11 22:15:17 -0500

    :fire: Added Link hrefs to 01 02 03 04 links :fire:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,90 +134,115 @@ export default function Home() {
                 </h2>
 
                 <div className="mt-28 grid gap-5 md:grid-cols-2 lg:grid-cols-4">
-                    <div className="flex flex-col justify-between bg-footer">
-                        <div className="space-y-4 px-4 pb-10 pt-4">
-                            <div className="flex items-start justify-between">
-                                <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
-                                    01
-                                </p>
-                                <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
-                                    <MoveUpRight size={18} />
+                    <Link href="/swap">
+                        <div className="hover:bg-hover-color flex h-full cursor-pointer flex-col justify-between bg-footer">
+                            <div className="space-y-4 px-4 pb-10 pt-4">
+                                <div className="flex items-start justify-between">
+                                    <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
+                                        01
+                                    </p>
+                                    <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
+                                        <MoveUpRight size={18} />
+                                    </div>
                                 </div>
+                                <p className="text-[15px] leading-6">
+                                    Traders swap tokens with minimal slippage
+                                    and pay some of the lowest fees to MONI
+                                    lockers.
+                                </p>
                             </div>
-                            <p className="text-[15px] leading-6">
-                                Traders swap tokens with minimal slippage and
-                                pay some of the lowest fees to MONI lockers.
-                            </p>
+                            <div className="bg-btn-primary pt-8">
+                                <Image
+                                    src={BEAR5}
+                                    alt="bear"
+                                    className="m-auto"
+                                />
+                            </div>
                         </div>
-                        <div className="bg-btn-primary pt-8">
-                            <Image src={BEAR5} alt="bear" className="m-auto" />
-                        </div>
-                    </div>
+                    </Link>
 
-                    <div className="flex flex-col justify-between bg-footer">
-                        <div className="space-y-4 px-4 pb-10 pt-4">
-                            <div className="flex items-start justify-between">
-                                <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
-                                    02
-                                </p>
-                                <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
-                                    <MoveUpRight size={18} />
+                    <Link href="/liquidity">
+                        <div className="hover:bg-hover-color flex h-full cursor-pointer flex-col justify-between bg-footer">
+                            <div className="space-y-4 px-4 pb-10 pt-4">
+                                <div className="flex items-start justify-between">
+                                    <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
+                                        02
+                                    </p>
+                                    <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
+                                        <MoveUpRight size={18} />
+                                    </div>
                                 </div>
+                                <p className="text-[15px] leading-6">
+                                    Liquidity providers deposit the tokens used
+                                    for trading on Moniswap and receive MONI
+                                    emissions as rewards.
+                                </p>
                             </div>
-                            <p className="text-[15px] leading-6">
-                                Liquidity providers deposit the tokens used for
-                                trading on Moniswap and receive MONI emissions
-                                as rewards.
-                            </p>
+                            <div className="bg-btn-primary pt-8">
+                                <Image
+                                    src={BEAR2}
+                                    alt="bear"
+                                    className="m-auto"
+                                />
+                            </div>
                         </div>
-                        <div className="bg-btn-primary pt-8">
-                            <Image src={BEAR2} alt="bear" className="m-auto" />
-                        </div>
-                    </div>
+                    </Link>
 
-                    <div className="flex flex-col justify-between bg-footer">
-                        <div className="space-y-4 px-4 pb-10 pt-4">
-                            <div className="flex items-start justify-between">
-                                <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
-                                    03
-                                </p>
-                                <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
-                                    <MoveUpRight size={18} />
+                    <Link href="/incentives">
+                        <div className="hover:bg-hover-color flex h-full cursor-pointer flex-col justify-between bg-footer">
+                            <div className="space-y-4 px-4 pb-10 pt-4">
+                                <div className="flex items-start justify-between">
+                                    <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
+                                        03
+                                    </p>
+                                    <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
+                                        <MoveUpRight size={18} />
+                                    </div>
                                 </div>
+                                <p className="text-[15px] leading-6">
+                                    Protocols incentivize veMONI voters to
+                                    attract votes and MONI emissions to their
+                                    pools, enabling them to build liquidity
+                                    cost-effectively.
+                                </p>
                             </div>
-                            <p className="text-[15px] leading-6">
-                                Protocols incentivize veMONI voters to attract
-                                votes and MONI emissions to their pools,
-                                enabling them to build liquidity
-                                cost-effectively.
-                            </p>
+                            <div className="bg-btn-primary pt-8">
+                                <Image
+                                    src={BEAR3}
+                                    alt="bear"
+                                    className="m-auto"
+                                />
+                            </div>
                         </div>
-                        <div className="bg-btn-primary pt-8">
-                            <Image src={BEAR3} alt="bear" className="m-auto" />
-                        </div>
-                    </div>
+                    </Link>
 
-                    <div className="flex flex-col justify-between bg-footer">
-                        <div className="space-y-4 px-4 pb-10 pt-4">
-                            <div className="flex items-start justify-between">
-                                <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
-                                    04
-                                </p>
-                                <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
-                                    <MoveUpRight size={18} />
+                    <Link href="/vote">
+                        <div className="hover:bg-hover-color flex h-full cursor-pointer flex-col justify-between bg-footer">
+                            <div className="space-y-4 px-4 pb-10 pt-4">
+                                <div className="flex items-start justify-between">
+                                    <p className="font-barlow text-6xl text-content-gray opacity-[0.18]">
+                                        04
+                                    </p>
+                                    <div className="rounded-full bg-black1 p-3 text-arrow-yellow">
+                                        <MoveUpRight size={18} />
+                                    </div>
                                 </div>
+                                <p className="text-[15px] leading-6">
+                                    veMONI voters vote on which pools will earn
+                                    MONI emissions and receive all incentives
+                                    and fees. Any MONI holder can lock their
+                                    tokens to convert to veMONI.
+                                </p>
                             </div>
-                            <p className="text-[15px] leading-6">
-                                veMONI voters vote on which pools will earn MONI
-                                emissions and receive all incentives and fees.
-                                Any MONI holder can lock their tokens to convert
-                                to veMONI.
-                            </p>
+                            <div className="bg-btn-primary pt-8">
+                                <Image
+                                    src={BEAR4}
+                                    alt="bear"
+                                    className="m-auto"
+                                />
+                            </div>
                         </div>
-                        <div className="bg-btn-primary pt-8">
-                            <Image src={BEAR4} alt="bear" className="m-auto" />
-                        </div>
-                    </div>
+                    </Link>
                 </div>
 
                 <div className="mt-20 space-y-10 pb-20 text-center">
