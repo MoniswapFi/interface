a6dbd219e921dbcbeb760b6d4d59adccc33a56f6
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-09-11 20:26:50 -0500

    :zap: Cut footer central padding in half :zap:

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     const today = new Date().getFullYear();
     return (
         <div className="bg-footer px-10 py-5">
-            <div className="flex items-end justify-between pb-[70px] md:items-start md:pb-[105px]">
+            <div className="flex items-end justify-between pb-[35px] md:items-start md:pb-[55px]">
                 <div className="flex flex-col gap-3 md:flex-row md:items-center md:gap-5">
                     <Link href={"#"}>Documentation</Link>
                     <Link href={"#"}>Security</Link>
@@ -16,7 +16,7 @@ export const Footer = () => {
                 <div className="bg-btn-primary px-5 py-3">Mirror &lt;</div>
             </div>
 
-            <div className="justify-between max-md:space-y-10 max-md:pb-5 md:flex">
+            <div className="max-md:space-y-10max-md:pb-5 justify-between md:flex">
                 <div className="flex flex-col md:flex-row">
                     <span>{today} &copy; Moniswap</span>
                     <span>Terms of Service</span>
