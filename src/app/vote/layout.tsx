9f105f7ea1576e7f215990fa73949fc6887179ff
Watson Lewis-Rodriguez <38282105+5eh@users.noreply.github.com>
2024-09-12 15:57:10 -0500

    :fire: Added Vote Layout :fire:

---
+++ src/app/vote/layout.tsx
@@ -0,0 +1,20 @@
+import {
+    DEFAULT_TITLE,
+    TITLE_BREAKER,
+    VOTE_DESCRIPTION,
+    VOTE_TITLE,
+} from "@/components/configuration/seo";
+import * as React from "react";
+
+export const metadata = {
+    title: `${VOTE_TITLE} ${TITLE_BREAKER} ${DEFAULT_TITLE}`,
+    description: `${VOTE_DESCRIPTION}`,
+};
+
+export default function ComponentsLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return <>{children}</>;
+}
